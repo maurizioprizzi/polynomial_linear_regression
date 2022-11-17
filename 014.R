07f525abe68517f7272e2021a026e3b6df90d705
Maurizio Prizzi <maurizioprizzi@gmail.com>
2022-11-16 21:10:38 -0300

    Polynomial regression (to the 4th power)

--- 014.R
+++ 014.R
@@ -2,3 +2,37 @@
 
 # Importing the dataset
 dataset = read.csv("Position_Salaries.csv")
+dataset = dataset[2:3]
+
+# Fitting Linear Regression to the dataset
+lin_reg = lm(formula = Salary ~ ., 
+             data = dataset)
+
+# Fitting Polynomial Regression to the dataset
+dataset$Level2 = dataset$Level^2
+dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
+poly_reg = lm(formula = Salary ~ ., 
+              data = dataset)
+
+# Visualizing the Linear Regression results
+# install.packages("ggplot2")
+library(ggplot2)
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = "red") + 
+  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
+            colour = "blue") +
+  ggtitle("Truth or Bluff (Linear Regression)") +
+  xlab("Level") +
+  ylab("Salary")
+
+# Visualizing the Polynomial regression model
+ggplot() + 
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = "red") + 
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
+            colour = "blue") +
+  ggtitle("Truth or Bluff (Polynomial Regression)") +
+  xlab("Level") +
+  ylab("Salary")
